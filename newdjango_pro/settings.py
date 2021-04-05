490c21a3ebf7b966b9501ed6f802d0db7128e38b
shijinsidharth <shijinsidharth98@gmail.com>
2021-04-05 13:25:19 +0530

    first commit

--- newdjango_pro/settings.py
+++ newdjango_pro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2yg#v_l_=%_!+hkl*3ks7qk$56p&k8k3022&b%bkl3s)04!sdg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
